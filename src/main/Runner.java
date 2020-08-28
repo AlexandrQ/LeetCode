fde875912ebde7fd29ba770a3f0011cf3c67e66c
Alex <alexandr.rusanov1@gmail.com>
2020-08-28 09:10:52 +0300

    Roman To Integer

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -2,12 +2,21 @@ package main;
 
 import main.easy.PalindromeNumber;
 import main.easy.ReverseInteger;
+import main.easy.RomanToInteger;
 import main.easy.TwoSum;
 
 public class Runner {
     public static void main(String[] args){
         //Insert code here
-        PalindromeNumber palindromeNumber = new PalindromeNumber();
-        System.out.println(palindromeNumber.isPalindrome(121));
+        RomanToInteger obj = new RomanToInteger();
+        System.out.println(obj.romanToInt("IXIX"));
+        System.out.println(obj.romanToInt("IV"));
+        System.out.println(obj.romanToInt("XL"));
+        System.out.println(obj.romanToInt("CM"));
+        System.out.println(obj.romanToInt("I"));
+        System.out.println(obj.romanToInt("II"));
+        System.out.println(obj.romanToInt("III"));
+        System.out.println(obj.romanToInt("LVIII"));
+        System.out.println(obj.romanToInt("MCMXCIV"));
     }
 }
--- src/main/easy/TwoSum.java
+++ src/main/easy/TwoSum.java
@@ -1,18 +1,20 @@
 package main.easy;
 
+/*
+    Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    Example:
+
+    Given nums = [2, 7, 11, 15], target = 9,
+
+    Because nums[0] + nums[1] = 2 + 7 = 9,
+    return [0, 1].
+*/
+
 public class TwoSum {
-    /*
-        Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
-        You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-        Example:
-
-        Given nums = [2, 7, 11, 15], target = 9,
-
-        Because nums[0] + nums[1] = 2 + 7 = 9,
-        return [0, 1].
-    */
     public int[] easyWay(int[] nums, int target) {
         int[] result = {0, 0};
         for(int i = 0; i < nums.length-1; i++) {
