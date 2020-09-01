81860a679a079109c17f05127bb63329b49768ac
Alex <alexandr.rusanov1@gmail.com>
2020-09-01 09:43:39 +0300

    Longest Common Prefix

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -1,22 +1,12 @@
 package main;
 
-import main.easy.PalindromeNumber;
-import main.easy.ReverseInteger;
-import main.easy.RomanToInteger;
-import main.easy.TwoSum;
+import main.easy.*;
 
 public class Runner {
     public static void main(String[] args){
         //Insert code here
-        RomanToInteger obj = new RomanToInteger();
-        System.out.println(obj.romanToInt("IXIX"));
-        System.out.println(obj.romanToInt("IV"));
-        System.out.println(obj.romanToInt("XL"));
-        System.out.println(obj.romanToInt("CM"));
-        System.out.println(obj.romanToInt("I"));
-        System.out.println(obj.romanToInt("II"));
-        System.out.println(obj.romanToInt("III"));
-        System.out.println(obj.romanToInt("LVIII"));
-        System.out.println(obj.romanToInt("MCMXCIV"));
+        LongestCommonPrefix obj = new LongestCommonPrefix();
+        String[] strs = {"abab","aba", ""};
+        System.out.println(obj.longestCommonPrefix(strs));
     }
 }
