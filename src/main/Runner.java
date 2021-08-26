f739753efa8af5cff88d6f7ca2bbda2eb8c04a85
Alex <alexandr.rusanov1@gmail.com>
2021-08-26 13:05:26 +0300

    RemoveDuplicatesFromSortedList

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -2,33 +2,14 @@ package main;
 
 import main.easy.*;
 
-import java.util.Arrays;
-
 public class Runner {
     public static void main(String[] args){
         //Insert code here
-        ClimbingStairs obj = new ClimbingStairs();
 
-        /*System.out.println(obj.climbStairs(1));
-        System.out.println(obj.climbStairs(2));
-        System.out.println(obj.climbStairs(3));
+        RemoveDuplicatesFromSortedList obj = new RemoveDuplicatesFromSortedList();
+        System.out.println(obj.deleteDuplicates(RemoveDuplicatesFromSortedList.getListNodes(new int[] { 1, 1, 2 })).printAllValues());
+        System.out.println(obj.deleteDuplicates(RemoveDuplicatesFromSortedList.getListNodes(new int[] { 1, 1, 2, 3, 3 })).printAllValues());
 
-        System.out.println(obj.climbStairs(5));
-        System.out.println(obj.climbStairs(6));
-        System.out.println(obj.climbStairs(7));
-        System.out.println(obj.climbStairs(8));*/
-
-        System.out.println(obj.climbStairs(35));
-
-
-
-        /*System.out.println(obj.maxSubArray(new int[] { -2,1,-3,4,-1,2,1,-5,4 }));
-        System.out.println(obj.maxSubArray(new int[] { 1 }));
-        System.out.println(obj.maxSubArray(new int[] { 5,4,-1,7,8 }));
-        System.out.println(obj.maxSubArray(new int[] { -2,-1 }));
-        System.out.println(obj.maxSubArray(new int[] { 2,-3,0,3 }));
-        System.out.println(obj.maxSubArray(new int[] { 2,-1,-3,-1,3,0,-1,-3,3,0 }));
-        System.out.println(obj.maxSubArray(new int[] { 3,-2,-3,-3,1,3,0 }));
-        System.out.println(obj.maxSubArray(new int[] { -3,-2,0,-1 }));*/
+        /*System.out.println(obj.maxSubArray(new int[] { -2,1,-3,4,-1,2,1,-5,4 }));*/
     }
 }
