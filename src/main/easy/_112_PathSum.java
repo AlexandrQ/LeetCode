9c9d5c14dea900024c2e94b8901f182c43765eee
Alex <alexandr.rusanov1@gmail.com>
2021-09-11 22:02:04 +0300

    PathSum

---
+++ src/main/easy/_112_PathSum.java
@@ -0,0 +1,37 @@
+package main.easy;
+
+/*
+    Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+    A leaf is a node with no children.
+*/
+
+public class _112_PathSum {
+    private boolean result = false;
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) {
+            return false;
+        }
+        hasPathSum(root, 0, targetSum);
+        return result;
+    }
+
+    private void hasPathSum(TreeNode node, int currentSum, int targetSum) {
+        if(!result && node != null) {
+            if(node.left != null) {
+                hasPathSum(node.left, currentSum + node.val, targetSum);
+            }
+            if(node.right != null) {
+                hasPathSum(node.right, currentSum + node.val, targetSum);
+            }
+            if(node.left == null && node.right == null && currentSum + node.val == targetSum) {
+                result = true;
+            }
+        }
+    }
+}
+
+/*
+    Runtime: 0 ms, faster than 100.00% of Java online submissions for Path Sum.
+    Memory Usage: 41 MB, less than 8.47% of Java online submissions for Path Sum.
+*/
