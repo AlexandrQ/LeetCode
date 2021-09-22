3856ad95f3f5de8f77672983d0bb42407046f74b
Alex <alexandr.rusanov1@gmail.com>
2021-09-22 15:37:31 +0300

    Add description

--- src/main/easy/_167_TwoSumII_InputArrayIsSorted.java
+++ src/main/easy/_167_TwoSumII_InputArrayIsSorted.java
@@ -1,5 +1,38 @@
 package main.easy;
 
+/*
+    Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= first < second <= numbers.length.
+
+    Return the indices of the two numbers, index1 and index2, as an integer array [index1, index2] of length 2.
+
+    The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+    Example 1:
+
+    Input: numbers = [2,7,11,15], target = 9
+    Output: [1,2]
+    Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+    Example 2:
+
+    Input: numbers = [2,3,4], target = 6
+    Output: [1,3]
+    Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3.
+
+    Example 3:
+
+    Input: numbers = [-1,0], target = -1
+    Output: [1,2]
+    Explanation: The sum of 1 and 0 is -1. Therefore index1 = 1, index2 = 2.
+
+    Constraints:
+
+    2 <= numbers.length <= 3 * 104
+    -1000 <= numbers[i] <= 1000
+    numbers is sorted in non-decreasing order.
+    -1000 <= target <= 1000
+    The tests are generated such that there is exactly one solution.
+*/
+
 public class _167_TwoSumII_InputArrayIsSorted {
     public int[] twoSum(int[] numbers, int target) {
         int[] res = new int[2];
@@ -34,3 +67,8 @@ public class _167_TwoSumII_InputArrayIsSorted {
         return -1;
     }
 }
+
+/*
+    Runtime: 2 ms, faster than 27.90% of Java online submissions for Two Sum II - Input array is sorted.
+    Memory Usage: 39.3 MB, less than 59.96% of Java online submissions for Two Sum II - Input array is sorted.
+*/
