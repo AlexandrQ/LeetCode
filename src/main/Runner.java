69551cbc5b55cbc5e40ec82d252a5fbf4f6e838a
Alex <alexandr.rusanov1@gmail.com>
2021-09-30 08:34:22 +0300

    ExcelSheetColumnNumber

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -8,10 +8,11 @@ public class Runner {
     public static void main(String[] args){
         //Insert code here
 
-        _136_SingleNumber obj = new _136_SingleNumber();
-        System.out.println(obj.singleNumber(new int[] { 2,2,1 }));
-        System.out.println(obj.singleNumber(new int[] { 4,1,2,1,2 }));
-        System.out.println(obj.singleNumber(new int[] { 4,4,1,2,1,0,0,8,2 }));
+        _171_ExcelSheetColumnNumber obj = new _171_ExcelSheetColumnNumber();
+        System.out.println(">>>FXSHRXW 2147483647 | " + obj.titleToNumber("FXSHRXW")); //2147483647
+        //System.out.println(obj.titleToNumber("ZY"));
+        System.out.println(">>>AAA 703 | " + obj.titleToNumber("AAA"));
+        System.out.println(">>>ZZ 702 | " + obj.titleToNumber("ZZ"));
 
 
         /*System.out.println(obj.maxSubArray(new int[] { -2,1,-3,4,-1,2,1,-5,4 }));*/
