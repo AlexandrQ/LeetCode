ef7064de395ccdc00d4c806d4cc1e16e4a2d017b
Alex <alexandr.rusanov1@gmail.com>
2021-10-13 18:49:18 +0300

    ZigZagConversion

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -4,16 +4,17 @@ import main.java.easy.*;
 import main.java.medium._2_AddTwoNumbers;
 import main.java.medium._3_LongestSubstringWithoutRepeatingCharacters;
 import main.java.medium._5_LongestPalindromicSubstring;
+import main.java.medium._6_ZigZagConversion;
 
 public class Runner {
     public static void main(String[] args){
         //Insert code here
 
-        _5_LongestPalindromicSubstring obj = new _5_LongestPalindromicSubstring();
+        _6_ZigZagConversion obj = new _6_ZigZagConversion();
         //System.out.println(obj.reverseBits(00000010100101000001111010011100));
         //System.out.println(obj.reverseBits(43261596));
         //System.out.println(obj.longestPalindrome("babad"));
-        System.out.println(obj.longestPalindrome("aaaaa"));
+        System.out.println(obj.convert("PAYPALISHIRING", 4));
 
 
 
