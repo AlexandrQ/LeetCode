297608c04ba07fc6741a251ec5329aa9a1da1ca4
Alex <alexandr.rusanov1@gmail.com>
2021-10-16 08:56:21 +0300

    StringToIntegerAtoi ugly solution

--- src/main/Runner.java
+++ src/main/Runner.java
@@ -7,11 +7,11 @@ public class Runner {
     public static void main(String[] args){
         //Insert code here
 
-        var obj = new _279_PerfectSquares();
+        var obj = new _8_StringToIntegerAtoi();
         //System.out.println(obj.reverseBits(00000010100101000001111010011100));
         //System.out.println(obj.reverseBits(43261596));
         //System.out.println(obj.longestPalindrome("babad"));
-        System.out.println(obj.numSquares(7168));
+        System.out.println(obj.myAtoi("   -42"));
 
 
 
