8d06a7a2c7608c3ba4834b5fe0149f842372327d
Alex <alexandr.rusanov1@gmail.com>
2022-05-09 12:10:56 +0300

    Rotate Image

---
+++ src/main/java/medium/_48_RotateImage.java
@@ -0,0 +1,46 @@
+package main.java.medium;
+
+/*
+    You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+    You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    Output: [[7,4,1],[8,5,2],[9,6,3]]
+    Example 2:
+
+    Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+    Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+    Constraints:
+
+    n == matrix.length == matrix[i].length
+    1 <= n <= 20
+    -1000 <= matrix[i][j] <= 1000
+*/
+
+public class _48_RotateImage {
+    public void rotate(int[][] matrix) {
+        final int maxCycleCount = matrix.length%2 == 0 ? matrix.length / 2 : (matrix.length - 1) / 2;
+        final int martixLength = matrix.length - 1;
+        for(int cycleCount = 0; cycleCount < maxCycleCount; cycleCount++) {
+            for(int j = cycleCount; j < martixLength - cycleCount; j++) {
+                int tempTop = matrix[j][martixLength - cycleCount];
+                matrix[j][martixLength - cycleCount] = matrix[cycleCount][j];
+
+                int tempRight = matrix[martixLength - cycleCount][martixLength - j];
+                matrix[martixLength - cycleCount][martixLength - j] = tempTop;
+
+                int tempBottom = matrix[martixLength - j][cycleCount];
+                matrix[martixLength - j][cycleCount] = tempRight;
+
+                matrix[cycleCount][j] = tempBottom;
+            }
+        }
+    }
+}
+
+/*
+    Runtime: 0 ms, faster than 100.00% of Java online submissions for Rotate Image.
+    Memory Usage: 42.9 MB, less than 44.13% of Java online submissions for Rotate Image.
+*/
